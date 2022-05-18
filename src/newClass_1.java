38c3311a1ac29f39e570c48299ddb6c769d3b0c7
Alexx2405 <alexxlinuxx@gmail.com>
2022-05-18 20:58:25 +0200

    d

---
+++ src/newClass_1.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alex
+ */
+/*
+-----------
+requisitos:
+-----------
+
+-----------
+pseucódigo:
+-----------
+
+
+
+*/
+import java.util.Scanner;
+public class newClass_1 {
+   public static void main (String[] args) {
+
+    // Declaración de objeto Scanner para petición de datos por teclado
+    Scanner input= new Scanner (System.in);
+                
+    // Declaración de variables ("cajas" donde puedo guardar información)
+    //CONSTANTES    
+    
+    // Variables de entrada
+	
+	
+	
+    // Variables de salida
+	
+	
+	//Variables Auxiliares
+	
+	
+    
+    
+
+    //--------------------
+    // Entrada de datos 
+    //--------------------
+    // Mensaje de cabecera
+    
+    
+    // Introducción de los datos de entrada
+    
+    
+	
+    
+    //-----------------------
+    //      Procesamiento   
+    //-----------------------
+	
+    
+    //-------------------------
+    // Salida de Resultados 
+    //-------------------------
+	    
+		
+    //FIN
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
+
